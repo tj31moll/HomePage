77c4933cedd2b98eb7f9f8732cd4dc987e98b24a
tj31moll <91799649+tj31moll@users.noreply.github.com>
2023-03-21 17:22:38 -0400

    Update appsub.py

--- template/appsub.py
+++ template/appsub.py
@@ -1,15 +1,9 @@
 from flask import Flask, render_template, request
+from sort_drive import sort_drive_files
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-from flask import Flask, render_template, request
-from sort_drive import sort_drive_files
-
-# other code in app.py
+# Your existing code for the homepage, Google Calendar, and OneNote integration
 
 @app.route('/subpage', methods=['GET', 'POST'])
 def subpage():
@@ -26,5 +20,12 @@ def subpage():
 
     return render_template('subpage.html', result=result)
 
+@app.route('/csv_to_sql', methods=['GET', 'POST'])
+def csv_to_sql():
+    # Your existing code for uploading CSV and converting it to SQL
+
+# Add the following line to your main() function
+app.run(host='0.0.0.0', port=3666)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    main()
