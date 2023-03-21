bd99952bca20616b7e8afe638ff8a6e364629b9d
tj31moll <91799649+tj31moll@users.noreply.github.com>
2023-03-21 17:53:55 -0400

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,1 +1,87 @@
 
+// Replace with your own API keys and credentials
+const GOOGLE_API_KEY = 'YOUR_GOOGLE_API_KEY';
+const GOOGLE_CLIENT_ID = 'YOUR_GOOGLE_CLIENT_ID';
+const MICROSOFT_APP_ID = 'YOUR_MICROSOFT_APP_ID';
+const MICROSOFT_APP_SECRET = 'YOUR_MICROSOFT_APP_SECRET';
+
+// Google Calendar integration
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+}
+
+function initClient() {
+    gapi.client.init({
+        apiKey: GOOGLE_API_KEY,
+        clientId: GOOGLE_CLIENT_ID,
+        discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
+        scope: "https://www.googleapis.com/auth/calendar.readonly"
+    }).then(() => {
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    }, (error) => {
+        console.log(JSON.stringify(error, null, 2));
+    });
+}
+
+function updateSigninStatus(isSignedIn) {
+    const authorizeButton = document.getElementById('authorize_button');
+    const signoutButton = document.getElementById('signout_button');
+    const calendarContent = document.getElementById('calendar_content');
+
+    if (isSignedIn) {
+        authorizeButton.style.display = 'none';
+        signoutButton.style.display = 'block';
+        calendarContent.style.display = 'block';
+        listUpcomingEvents();
+    } else {
+        authorizeButton.style.display = 'block';
+        signoutButton.style.display = 'none';
+        calendarContent.style.display = 'none';
+    }
+
+    authorizeButton.onclick = handleAuthClick;
+    signoutButton.onclick = handleSignoutClick;
+}
+
+function handleAuthClick() {
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+function handleSignoutClick() {
+    gapi.auth2.getAuthInstance().signOut();
+}
+
+function listUpcomingEvents() {
+    gapi.client.calendar.events.list({
+        'calendarId': 'primary',
+        'timeMin': (new Date()).toISOString(),
+        'showDeleted': false,
+        'singleEvents': true,
+        'maxResults': 10,
+        'orderBy': 'startTime'
+    }).then((response) => {
+        const events = response.result.items;
+        const calendarContent = document.getElementById('calendar_content');
+
+        if (events.length > 0) {
+            calendarContent.innerHTML = '<ul>';
+            for (let i = 0; i < events.length; i++) {
+                const event = events[i];
+                const start = event.start.dateTime || event.start.date;
+                calendarContent.innerHTML += `<li>${start} - ${event.summary}</li>`;
+            }
+            calendarContent.innerHTML += '</ul>';
+        } else {
+            calendarContent.innerHTML = 'No upcoming events found.';
+        }
+    });
+}
+
+// OneNote integration
+document.getElementById('onenote_button').addEventListener('click', () => {
+    // Add your OneNote authorization and API calls here
+});
+
+// Load the Google Calendar API client
+document.addEventListener('DOMContentLoaded', handleClientLoad);
