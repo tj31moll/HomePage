5d0d9d2f3ab5b192388ac2f80b23044479043f07
tj31moll <91799649+tj31moll@users.noreply.github.com>
2023-03-21 17:28:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for
+from sort_drive import sort_drive_files
 import pandas as pd
 import sqlite3
 from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# Your existing code for the homepage, Google Calendar, and OneNote integration
+
+@app.route('/subpage', methods=['GET', 'POST'])
+def subpage():
+    result = None
+    if request.method == 'POST':
+        # Call your Python script here and store the result in the 'result' variable
+        try:
+            credentials_file = 'credentials.json'
+            folder_id = 'your_folder_id_here'
+            sort_drive_files(credentials_file, folder_id)
+            result = "Successfully sorted files in Google Drive folder."
+        except Exception as e:
+            result = f"An error occurred: {str(e)}"
+
+    return render_template('subpage.html', result=result)
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -34,5 +48,8 @@ def view_data():
 
     return redirect(url_for('index'))
 
+# Add the following line to your main() function
+app.run(host='0.0.0.0', port=3666)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    main()
